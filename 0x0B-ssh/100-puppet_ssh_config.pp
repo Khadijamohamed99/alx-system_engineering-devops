cabcf68a4d3b8a45f614ab95ac4f5a5a08a5f20e
Khadijamohamed99 <khadijamohamed5220@gmail.com>
2023-09-26 14:34:12 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/user/bin/env bash
 # using puppet to make changes yo our configuration file
 
-file { '/ect/ssh/ssh_config':
+file {'ect/ssh/ssh_config':
 ensure  => present,
 content =>"
 #ssh client configuration
