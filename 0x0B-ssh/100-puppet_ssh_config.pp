7d838418977541e8079f59feb18728160390035d
Khadijamohamed99 <khadijamohamed5220@gmail.com>
2023-09-26 14:32:23 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/user/bin/env bash
 # using puppet to make changes yo our configuration file
 
-file {'ect/ssh/ssh_config':
+file { '/ect/ssh/ssh_config':
 ensure  => present,
 content =>"
 #ssh client configuration
 host*
-IdentityFile ~/.ssh/school_rsa
+IdentityFile ~/.ssh/school
 PasswordAuthentication no
 ",
 }
