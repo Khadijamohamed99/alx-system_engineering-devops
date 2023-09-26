3bf1c3302c5885e18ea4bffbbdca3646fd25b8ee
Khadijamohamed99 <khadijamohamed5220@gmail.com>
2023-09-26 13:41:15 -0700

    ssh key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
+#!/usr/bin/env bash
 # Seting up my client config file
-include stdlib
 
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  replace => true,
-}
+file {'ect/ssh/ssh_cofig':
+	ensure => present,
+content =>"
 
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school_rsa
+	PasswordAuthentication no
+
 }
